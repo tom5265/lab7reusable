8fbffd0db043a7b5cdd8141337eef6c9f73a2bd6
Tom Moore <tom5265@gmail.com>
2016-03-02 23:00:37 -0600

    struggling

--- reusableTemplate.js
+++ reusableTemplate.js
@@ -1,69 +1,107 @@
-var library = (function(){
-	return {
-		// Utility --- Complete Functions Below
-		identity : function(val) {},
+var library = (function () {
+    return {
+        // Utility --- Complete Functions Below
+        identity: function (val) {
+            return val;
+        },
 
-		// Collections --- Complete Functions Below
-		each : function(list, iterator) {},
+        // Collections --- Complete Functions Below
+        each: function (list, iterator) {
+            list = ['dog', 'cat', 'fish'];
+            for (var i = 0; i < list.length; i++) {
+                iterator(list[i], i, list);
+            }
 
-		filter : function(list, test) {},
+        },
 
-		reject : function(list, test) {},
+        filter: function (list, test) {
+            var results = [];
+            
+            for (var i = 0; i < list.length; i++) {
+                if (list[i] > test) {
+                    results.push(list[i]);
+                }
+            }
+            return results;
+        },
+        reject: function (list, test) { },
 
-		map : function(list, iterator) {},
+        map: function (list, iterator) {
+            var array = [];
+            for (i = 0; i < list.length; i++) {
+                array.push(iterator(list[i], i, list));
+            }
+            return array;
+        },
 
-		pluck : function(list, key) {
-			return this.map(list, function(item){
-				return item[key];
-			});
-		},
-		reduce : function(list, iterator, accumulator) {},
+        pluck: function (list, key) {
+            return this.map(list, function (item) {
+                return item[key];
+            });
+        },
+        reduce: function (list, iterator, accumulator) {
+        for (var i = 0; i < list.length; i++) {
+            accumulator = iterator(list[i], accumulator);
+        }
+        return accumulator;
+        },
 
-		every : function(list, iterator) {},
+        every: function (list, iterator) { },
 
-		some : function(list, iterator) {},
+        some: function (list, iterator) { },
 
-		contains : function(list, target) {},
+        contains: function (list, target) {
+            for (var e in list) {
+                if (list[e] == target) {
+                    return true;
+                }
+            }
+            return false;
+        },
 
-		// Advanced Collections --- Complete Functions Below
-		shuffle : function(array) {},
+        // Advanced Collections --- Complete Functions Below
+        shuffle: function (array) { },
 
-		invoke : function(list, methodName, args) {},
+        invoke: function (list, methodName, args) { },
 
-		sortBy : function(list, iterator) {},
+        sortBy: function (list, iterator) { },
 
-		// Objects --- Complete Functions Below
-		extend : function(obj) {},
+        // Objects --- Complete Functions Below
+        extend: function (obj) { },
 
-		defaults : function(obj) {},
+        defaults: function (obj) { },
 
-		// Arrays --- Complete Functions Below
-		first : function(array, n) {
-			return n === undefined ? array[0] : array.slice(0, n);
-		},
+        // Arrays --- Complete Functions Below
+        first: function (array, n) {
+            return n === undefined ? array[0] : array.slice(0, n);
+        },
 
-		last : function(array, n) {},
+        last: function (array, n) { },
 
-		indexOf : function(array, target){},
+        indexOf: function (array, target) {
+            return array.indexOf(target);
+        },
 
-		uniq : function(array) {},
+        uniq: function (array) { },
 
-		// Advanced Arrays --- Complete Functions Below
-		zip : function() {},
+        // Advanced Arrays --- Complete Functions Below
+        zip: function () { },
 
-		flatten : function(nestedArray, result) {},
+        flatten: function (nestedArray, result) { },
 
-		intersection : function() {},
+        intersection: function () { },
 
-		difference : function(array) {},
+        difference: function (array) { },
 
-		// Functions --- Complete Functions Below
-		once : function(func) {},
+        // Functions --- Complete Functions Below
+        once: function (func) { },
 
-		memoize : function(func) {},
+        memoize: function (func) {
 
-		delay : function(func, wait) {}
-	}
+        },
+
+        delay: function (func, wait) { }
+    }
 })();
 
 
